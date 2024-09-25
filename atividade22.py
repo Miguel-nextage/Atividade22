78fab036c5498759dbc77e1b71cd20dd3d920f75
Miguel-nextage <Pmiguelhkss@gmail.com>
2024-09-25 13:51:51 -0300

    Soma de varios numeros

---
+++ atividade22.py
@@ -0,0 +1,23 @@
+# Crie um programa que receba vários números do usuário e some-os até que o número 0 seja digitado, momento em que o programa deve exibir o valor total.
+
+soma = 0
+
+while True:
+     
+    num  = int(input("Digite seu numero (0 para parar)"))
+    if num == 0:
+      break
+
+
+    soma += num
+   
+
+
+ 
+
+print(f"{soma}")
+
+
+
+
+    
